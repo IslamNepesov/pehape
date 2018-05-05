4260a7f83af6e8f07dd63bca0868392c2ce16092
pchizhov <32107377+pchizhov@users.noreply.github.com>
2018-05-05 18:16:27 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$user = 'root';
+$password = 'root';
+$db = 'k3140_chi';
+$host = 'localhost:8889';
+
+?>
+
---
+++ search.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'connect.php';
+ 
+$link = mysqli_connect($host, $user, $password, $db)
+or die ("Ошибка подключения к базе данных" . mysqli_error());
+
+$name = strtr($_GET['name'], '*', '%');
+$education = strtr($_GET['education'], '*', '%');
+
+echo "<form method='GET' action='search.php'>
+<p>Введите фамилию сторудника: <input type='text' name='name' value='$name'></p>
+<p>Введите образование сотрудника: <input type='text' name='education' value='$education'></p>
+<p><input type='submit' name='enter' value='Поиск'></p>
+</form>";
+
+if (isset($_GET['enter'])) {
+    $sql="SELECT name, education
+    FROM employee
+    WHERE name LIKE '%$name%' AND education LIKE '%$education%'";
+
+    $result = mysqli_query($link, $sql);
+    echo "<table border='1'>
+    <tr> 
+    <th>name</th>
+    <th>education</th>
+    </tr>";
+    while($row = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        echo "<td>" . $row['name'] . "</td>";
+        echo "<td>" . $row['education'] . "</td>";
+        echo "</tr>";
+    }
+echo "</table>";
+}
+
+mysqli_close($link);
+?>
